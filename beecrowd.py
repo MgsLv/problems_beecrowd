62ce0b0d661870ec984b1c8164e77c077d22990a
Miguel Soares <162351041+MgsLv@users.noreply.github.com>
2026-03-02 18:39:22 -0300

    Add files via upload

---
+++ beecrowd.py
@@ -0,0 +1,20 @@
+def problema1000():
+    print('Hello World!')
+
+def problema1001(a, b):
+    a = int(input())
+    b = int(input())
+    x = a + b
+    print('X =', x)
+
+def problema1002():
+    pi = 3.14159
+    raio = float(input())
+    area = pi * (raio*raio)
+    print("A=%.4f" % area)
+    
+def main():
+    problema1000()
+
+if __name__ == "__main__":
+    main()
