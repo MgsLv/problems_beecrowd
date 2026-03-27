ad51e2e4386b8561c22a32cfec4bb9f1ebece628
Miguel Soares <162351041+MgsLv@users.noreply.github.com>
2026-03-27 18:30:52 -0300

    Update beecrowd.py

--- beecrowd.py
+++ beecrowd.py
@@ -7,11 +7,41 @@ def problema1001(a, b):
     x = a + b
     print('X =', x)
 
-def problema1002():
+def problema1002(raio):
     pi = 3.14159
     raio = float(input())
     area = pi * (raio*raio)
     print("A=%.4f" % area)
+
+def problema1052(mes):
+    mes = int(input())
+
+    if mes == 1:
+        print("January")
+    elif mes == 2:
+        print("February")
+    elif mes == 3:
+        print("March")
+    elif mes == 4: 
+        print("April")
+    elif mes == 5:
+        print("May")
+    elif mes == 6:
+        print("June")
+    elif mes == 7: 
+        print("July")
+    elif mes == 8: 
+        print("August")
+    elif mes == 9: 
+        print("September")
+    elif mes == 10: 
+        print("October")
+    elif mes == 11:
+        print("November")
+    elif mes == 12: 
+        print("December")
+    else:
+        print("Error")
     
 def main():
     problema1000()
